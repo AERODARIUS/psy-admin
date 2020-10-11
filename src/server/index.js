af67f4958b8610bba80296bdfa235918f9f800cf
Dario Cruz <dario.cruz@fundingcircle.com>
2020-10-11 02:22:16 -0300

    Fixing tests and enabling them

--- src/server/index.js
+++ src/server/index.js
@@ -46,42 +46,48 @@ const useIsLogedIn = () => {
 const LogInButton = () => {
   useEffect(() => {
     if (isFirebaseInit()) {
-      const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+      firebase
+      .auth()
+      .setPersistence(process.env.NODE_ENV === 'test' ? firebase.auth.Auth.Persistence.NONE : firebase.auth.Auth.Persistence.LOCAL)
+      .then(() => {
+        const ui = new firebaseui.auth.AuthUI(firebase.auth());
     
-      ui.start('#firebaseui-auth-container', {
-        signInOptions: [
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID
-        ],
-        signInSuccessUrl: process.env.REACT_APP_BASE_URL,
-        callbacks: {
-          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-            //var user = authResult.user;
-            // var credential = authResult.credential;
-            // var isNewUser = authResult.additionalUserInfo.isNewUser;
-            // var providerId = authResult.additionalUserInfo.providerId;
-            // var operationType = authResult.operationType;
-            // Do something with the returned AuthResult.
-            // Return type determines whether we continue the redirect automatically
-            // or whether we leave that to developer to handle.
-            return true;
-          },
-          // signInFailure callback must be provided to handle merge conflicts which
-          // occur when an existing credential is linked to an anonymous user.
-          signInFailure: function(error) {
-            // For merge conflicts, the error.code will be
-            // 'firebaseui/anonymous-upgrade-merge-conflict'.
-            if (error.code !== 'firebaseui/anonymous-upgrade-merge-conflict') {
-              return Promise.resolve();
+        ui.start('#firebaseui-auth-container', {
+          signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+          ],
+          signInSuccessUrl: process.env.REACT_APP_BASE_URL,
+          callbacks: {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+              //var user = authResult.user;
+              // var credential = authResult.credential;
+              // var isNewUser = authResult.additionalUserInfo.isNewUser;
+              // var providerId = authResult.additionalUserInfo.providerId;
+              // var operationType = authResult.operationType;
+              // Do something with the returned AuthResult.
+              // Return type determines whether we continue the redirect automatically
+              // or whether we leave that to developer to handle.
+              return true;
+            },
+            // signInFailure callback must be provided to handle merge conflicts which
+            // occur when an existing credential is linked to an anonymous user.
+            signInFailure: function(error) {
+              // For merge conflicts, the error.code will be
+              // 'firebaseui/anonymous-upgrade-merge-conflict'.
+              if (error.code !== 'firebaseui/anonymous-upgrade-merge-conflict') {
+                return Promise.resolve();
+              }
+              // The credential the user tried to sign in with.
+              var cred = error.credential;
+              // Copy data from anonymous user to permanent user and delete anonymous
+              // user.
+              // ...
+              // Finish sign-in after data is copied.
+              return firebase.auth().signInWithCredential(cred);
             }
-            // The credential the user tried to sign in with.
-            var cred = error.credential;
-            // Copy data from anonymous user to permanent user and delete anonymous
-            // user.
-            // ...
-            // Finish sign-in after data is copied.
-            return firebase.auth().signInWithCredential(cred);
           }
-        }
+        });
       });
     }
   });
